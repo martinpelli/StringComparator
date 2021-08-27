7f58f269dc2a47f401624090a93e4ed00ac211bb
Pelli <friedstudios@outlook.com>
2021-08-26 23:11:57 -0300

    interface created

--- Practico1-Ejercicio7-TDI/Form1.Designer.cs
+++ Practico1-Ejercicio7-TDI/Form1.Designer.cs
@@ -29,13 +29,95 @@ namespace Practico1_Ejercicio7_TDI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tittleLabel = new System.Windows.Forms.Label();
+            this.stringTextBox = new System.Windows.Forms.TextBox();
+            this.string2TextBox = new System.Windows.Forms.TextBox();
+            this.compareButton = new System.Windows.Forms.Button();
+            this.stringLabel = new System.Windows.Forms.Label();
+            this.string2Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tittleLabel
+            // 
+            this.tittleLabel.AutoSize = true;
+            this.tittleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tittleLabel.Location = new System.Drawing.Point(85, 56);
+            this.tittleLabel.Name = "tittleLabel";
+            this.tittleLabel.Size = new System.Drawing.Size(317, 31);
+            this.tittleLabel.TabIndex = 0;
+            this.tittleLabel.Text = "Comparador de Cadenas";
+            // 
+            // stringTextBox
+            // 
+            this.stringTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stringTextBox.Location = new System.Drawing.Point(60, 163);
+            this.stringTextBox.Name = "stringTextBox";
+            this.stringTextBox.Size = new System.Drawing.Size(170, 38);
+            this.stringTextBox.TabIndex = 1;
+            // 
+            // string2TextBox
+            // 
+            this.string2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.string2TextBox.Location = new System.Drawing.Point(268, 163);
+            this.string2TextBox.Name = "string2TextBox";
+            this.string2TextBox.Size = new System.Drawing.Size(177, 38);
+            this.string2TextBox.TabIndex = 2;
+            // 
+            // compareButton
+            // 
+            this.compareButton.Location = new System.Drawing.Point(156, 242);
+            this.compareButton.Name = "compareButton";
+            this.compareButton.Size = new System.Drawing.Size(163, 36);
+            this.compareButton.TabIndex = 3;
+            this.compareButton.Text = "Comparar";
+            this.compareButton.UseVisualStyleBackColor = true;
+            // 
+            // stringLabel
+            // 
+            this.stringLabel.AutoSize = true;
+            this.stringLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stringLabel.Location = new System.Drawing.Point(57, 143);
+            this.stringLabel.Name = "stringLabel";
+            this.stringLabel.Size = new System.Drawing.Size(173, 17);
+            this.stringLabel.TabIndex = 4;
+            this.stringLabel.Text = "Ingrese la primera cadena";
+            // 
+            // string2Label
+            // 
+            this.string2Label.AutoSize = true;
+            this.string2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.string2Label.Location = new System.Drawing.Point(265, 143);
+            this.string2Label.Name = "string2Label";
+            this.string2Label.Size = new System.Drawing.Size(180, 17);
+            this.string2Label.TabIndex = 5;
+            this.string2Label.Text = "Ingrese la segunda cadena";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(507, 344);
+            this.Controls.Add(this.string2Label);
+            this.Controls.Add(this.stringLabel);
+            this.Controls.Add(this.compareButton);
+            this.Controls.Add(this.string2TextBox);
+            this.Controls.Add(this.stringTextBox);
+            this.Controls.Add(this.tittleLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label tittleLabel;
+        private System.Windows.Forms.TextBox stringTextBox;
+        private System.Windows.Forms.TextBox string2TextBox;
+        private System.Windows.Forms.Button compareButton;
+        private System.Windows.Forms.Label stringLabel;
+        private System.Windows.Forms.Label string2Label;
     }
 }
 
